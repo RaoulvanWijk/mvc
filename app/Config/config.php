c284d71309767ccdad628d6ec030f2f6ec08728d
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-11-28 09:26:20 +0100

    Added a config.php will later switch to .env

---
+++ app/Config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Database parameters of SqlServer
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'Cookie');
+    define('DB_USER', 'root');
+    define('DB_PASS', NULL);
+
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT (Dynamic links)
+    define('URLROOT', 'localhost:8080');
+
+    //Sitename
+    define('SITENAME', 'Youre Site Name');
