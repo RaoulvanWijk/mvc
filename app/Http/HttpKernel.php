c9276ea6e3e0f803400bbcfa3994d18a6c422bc2
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-11-28 09:26:35 +0100

    Added a working router for the application

---
+++ app/Http/HttpKernel.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http;
+
+use Closure;
+
+// This class will be used to store all the routes and middleware
+class HttpKernel
+{
+  /**
+   * This will be used to store all the routes
+   */
+  private $routes = [
+    'GET' => [],
+    'POST' => [],
+    'PUT' => [],
+    'DELETE' => []
+  ];
+
+  /**
+   * This will be used to store all the routeMiddleware
+   */
+  private $routeMiddleware = [
+    "demo" => \App\Http\Middleware\DemoMiddleware::class,
+    "demo2" => \App\Http\Middleware\DemoMiddleware2::class
+  ];
+
+  /**
+   * This method will be used to register a route to the routes array
+   * @param string $method
+   * @param string $path
+   * @param Closure|array $callable
+   * @param string $name
+   * @return void
+   */
+  public function registerRoute(string $method, string $path, Closure | array $callable, string $name): void
+  {
+    if(str_ends_with($path, '/') && $path !== '/') {
+      $path = substr($path, 0, -1);
+    }
+    $this->routes[$method][$path] = [
+      'callable' => $callable,
+      'name' => $name
+    ];
+  }
+
+
+  /**
+   * This method will be used to handle the route requested by the user
+   * @param string $url
+   * @param string $method
+   * @return array|Closure
+   */
+  public function handleRoute(string $url, string $method): array|Closure
+  {
+    // Remove query string variables from URL (if any).
+    $url = parse_url($url, PHP_URL_PATH);
+
+
+
+    // check if $mehtod is a valid method
+    if (!in_array($method, ['GET', 'POST', 'PUT', 'DELETE'])) {
+      throw new \Exception("Invalid request method: $method");
+    }
+
+    // check if the route exists without params
+    if (isset($this->routes[$method][$url])) {
+      return [$this->routes[$method][$url]['callable'], []];
+    }
+
+    // check if the route exists with params
+    return self::verifyRouteWithParams($url, $method);
+  }
+
+
+  /**
+   * This method will be used to verify if the route exists with params
+   * @param string $url
+   * @param string $method
+   * @return array|Closure
+   */
+  private function verifyRouteWithParams(string $url, string $method): array|Closure
+  {
+    // check if the route exists with params
+    foreach ($this->routes[$method] as $route => $callable) {
+      $validUrl = $route;
+      $route = preg_replace('/\//', '\/', $route);
+      $route = preg_replace('/\{[a-zA-Z0-9]*\}/', '([a-zA-Z0-9-]+)', $route);
+      if (preg_match('/^' . $route . '$/', $url, $matches)) {
+        array_shift($matches);
+        // $this->routes[$method][$url]['params'] = $matches;
+        return [$this->routes[$method][$validUrl]['callable'], $matches];
+      }
+    }
+    // route not found
+    throw new \Exception("Route not found: $url");
+    die();
+  }
+
+  /**
+   * This method will return the route by name
+   * @param string $name
+   * @return string
+   */
+  public function getRouteByName(string $name): string
+  {
+    foreach ($this->routes as $method => $routes) {
+      foreach ($routes as $route => $callable) {
+        if ($callable['name'] == $name) {
+          return $route;
+        }
+      }
+    }
+    return "";
+  }
+
+
+  
+  public function middleware(string $name, $callable)
+  {
+    if(isset($this->routeMiddleware[$name])) {
+      $t = new $this->routeMiddleware[$name];
+      $t->handle("test", $callable);
+    }
+  }
+
+
+}
---
+++ app/Http/Route.php
@@ -0,0 +1,105 @@
+<?php
+namespace App\Http;
+
+use App\Application;
+use Closure;
+
+class Route
+{
+
+  private static string $prefix = "";
+
+    /**
+   * This method will be used to register a GET route
+   * @param string $path
+   * @param Closure|array $callable
+   * @param string $name
+   * @return void
+   */
+  public static function get(string $path, Closure|array $callable, string $name): void
+  {
+    Application::$httpKernel->registerRoute('GET', self::$prefix . $path, $callable, $name);
+  }
+
+  /**
+   * This method will be used to register a POST route
+   * @param string $path
+   * @param Closure|array $callable
+   * @param string $name
+   * @return void
+   */
+  public static function post(string $path, Closure|array $callable, string $name): void
+  {
+    Application::$httpKernel->registerRoute('POST', self::$prefix . $path, $callable, $name);
+  }
+
+  /**
+   * This method will be used to register a PUT route
+   * @param string $path
+   * @param Closure|array $callable
+   * @param string $name
+   * @return void
+   */
+  public static function put(string $path, Closure|array $callable, string $name): void
+  {
+    Application::$httpKernel->registerRoute('PUT', self::$prefix . $path, $callable, $name);
+  }
+
+  /**
+   * This method will be used to register a DELETE route
+   * @param string $path
+   * @param Closure|array $callable
+   * @param string $name
+   * @return void
+   */
+  public static function delete(string $path, Closure|array $callable, string $name): void
+  {
+    Application::$httpKernel->registerRoute('DELETE', self::$prefix . $path, $callable, $name);
+  }
+
+
+
+  /**
+   * This method will set the prefix for the routes in a group function
+   * @param string $prefix
+   * @return self
+   */
+  public static function prefix(string $prefix): self
+  {
+    self::$prefix = $prefix;
+    return new self();
+  }
+  
+  /**
+   * This method will be used to register a group of routes
+   * @param Closure $callable
+   * @return self
+   */
+  public static function group(Closure $callable): self
+  {
+    $callable();
+    self::$prefix = "";
+    return new self();
+  }
+  
+  // create a function that will register middleware to a route
+    public static function middleware(string $name, $callable)
+    {
+      Application::$httpKernel->middleware($name, $callable);
+      return new self;
+    }
+
+  /**
+   * This method will return the route by name
+   * @param string $name
+   * @return string
+   */
+  public static function route(string $name, array $params = []): string
+  {
+    $route = Application::$httpKernel->getRouteByName($name);
+    foreach ($params as $key => $value) {
+      $route = str_replace('{' . $key . '}', $value, $route);
+    }
+    return $route;
+  }
+}
