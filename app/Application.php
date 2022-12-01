0964ecc20999e83d60bf5d3884fde4863259df5a
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2022-12-01 16:47:17 +0100

    Fixed request class bug and Request classes do not need to end with request

--- app/Application.php
+++ app/Application.php
@@ -17,6 +17,8 @@ class Application
   // This will be used to store method that is needed
   private string $currentMethod;
 
+  private array $params;
+
   // This method will be used to instantiate the HttpKernel class as singleton
   public function __construct()
   {
@@ -31,14 +33,14 @@ class Application
   {
     $request = self::$httpKernel->handleRoute($_SERVER['REQUEST_URI'], self::getRequestMethod());
     if(is_null($request)) exit();
-    $params = $request[1];
+    $this->params = $request[1];
     if(is_array($request[0])) {
       $this->currentRequestClass = new $request[0][0]();
       $this->currentMethod = $request[0][1];
       self::validateRequest();
-      call_user_func_array([$this->currentRequestClass, $this->currentMethod], $params);
+      call_user_func_array([$this->currentRequestClass, $this->currentMethod], $this->params);
     } else {
-      $request[0]($params);
+      $request[0]($this->params);
     }
   }
 
@@ -60,15 +62,15 @@ class Application
    * by checking if the method of the controller needs a request class
    * @return void
    */
-  public function validateRequest(): void
+  public function validateRequest() : void
   {
     $reflectionClass = new \ReflectionClass($this->currentRequestClass);
     $reflectionMethod = $reflectionClass->getMethod($this->currentMethod);
     if(count($reflectionMethod->getParameters()) > 0) {
       // Check if the first parameter needs a request class
-      if(\str_ends_with($reflectionMethod->getParameters()[0]->getType()->getName(), "Request")) {
+      if(is_subclass_of($reflectionMethod->getParameters()[0]->getType()->getName(), 'App\Http\Requests\Request')) {
         $requestClass = $reflectionMethod->getParameters()[0]->getType()->getName();
-        array_unshift($params, new $requestClass(array_merge($_GET, $_POST)));
+        array_unshift($this->params, new $requestClass(array_merge($_GET, $_POST)));
       }
     }
   }
