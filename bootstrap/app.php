170a2c6b68da1381f0e03589eb1459c0237d0af5
RaoulvanWijk <72564782+RaoulvanWijk@users.noreply.github.com>
2023-01-04 22:41:55 +0100

    This file will create the neccesarry singletons
    This file will be used to store the singletons for the application.
    also added a exception handler so that you dont have to look in console for error message

---
+++ bootstrap/app.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Create a new application instance
+ */
+$app = new App\Application();
+
+/**
+ * Register the application bindings
+ */
+ $app->singleton(
+   App\Http\HttpKernel\Kernel::class,
+   App\Http\HttpKernel\Kernel::class
+ );
+
+ $app->singleton(
+   App\Http\HttpKernel\Router::class,
+   App\Http\HttpKernel\Router::class
+ );
+
+$loader = new \Twig\Loader\FilesystemLoader('../resources/views');
+$twig = new \Twig\Environment($loader, [
+    "cache" => false
+]);
+
+$app->singleton(
+    'twig',
+    $twig);
+
+/**
+ * Register an exception handler
+ * This will dump and die any exceptions
+ * so that you don't have to look in the console for the exception message
+ */
+set_exception_handler(function($exception) {
+  dd($exception);
+});
+
+/**
+ * Return the application instance
+ * @return App\Application
+ */
+return $app;
