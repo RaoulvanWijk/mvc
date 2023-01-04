aaf6e4d7fb335cd8f83589db99ad2a55ab1d59f7
RaoulvanWijk <72564782+RaoulvanWijk@users.noreply.github.com>
2023-01-04 22:37:19 +0100

    Added a container exception
    If the class in the container is not found it throws this exception

---
+++ app/Exceptions/Container/ContainerException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Exceptions\Container;
+
+use Psr\Container\ContainerExceptionInterface;
+
+class ContainerException extends \Exception implements ContainerExceptionInterface
+{
+} 
