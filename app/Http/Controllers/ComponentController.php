0a622389ad35b7f3b6758dedf5a08d7cbc3d6046
Raoul van Wijk <72564782+RaoulvanWijk@users.noreply.github.com>
2023-01-30 14:18:00 +0100

    removed dd

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -8,7 +8,6 @@ class ComponentController
 {
   public function handle($type)
   {
-    dd($_SERVER);
     $type = str_replace(' ', '',ucwords(str_replace('.', "\\", $type)));
     return app('App\\Components\\'. $type)->render(...Request::capture()->getParsedBody());
   }
