3fe49c26ac14679189df4ee7610985d179ed961c
Raoul van Wijk <raoul.van.wijk05@gmail.com>
2023-01-30 09:12:51 +0100

    added component controller

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -1,1 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\HttpKernel\Request;
+
+class ComponentController
+{
+  public function handle($type)
+  {
     dd($_SERVER);
+    $type = str_replace(' ', '',ucwords(str_replace('.', "\\", $type)));
+    return app('App\\Components\\'. $type)->render(...Request::capture()->getParsedBody());
+  }
+}
